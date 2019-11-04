0053fa66f7c1c9faab34b39e127f00ce55f5f1fe
John Sharp <johncolinsharp@gmail.com>
2019-11-04 08:38:42 +0000

    use new jtypes vector

--- engine.c
+++ engine.c
@@ -311,13 +311,13 @@ jintRect engineGetTextureRect(engine *e, juint textureId)
     if (textureId >= eng->numTextures)
     {
         fprintf(stderr, "Warning: requested texture that did not exist\n");
-        jintRect ret = {{0,0},{0,0}};
+        jintRect ret = {{{0,0}},{{0,0}}};
         return ret;
     }
 
-    jintRect rect = {bl: {0, 0}};
+    jintRect rect = {bl: {{0, 0}}};
     SDL_QueryTexture(eng->textures[textureId],
-            NULL, NULL, &rect.tr.x, &rect.tr.y);
+            NULL, NULL, &rect.tr.v[0], &rect.tr.v[1]);
 
     return rect;
 }
@@ -327,19 +327,19 @@ void engineSpriteRender(engine * e, sprite *sp)
     SDL_Rect dest;
     engineInternal * eng = (engineInternal *)e;
 
-    dest.x = sp->rect.bl.x;
-    dest.y = e->h - sp->rect.tr.y;
-    dest.w = sp->rect.tr.x - sp->rect.bl.x;
-    dest.h = sp->rect.tr.y - sp->rect.bl.y;
+    dest.x = sp->rect.bl.v[0];
+    dest.y = e->h - sp->rect.tr.v[1];
+    dest.w = sp->rect.tr.v[0] - sp->rect.bl.v[0];
+    dest.h = sp->rect.tr.v[1] - sp->rect.bl.v[1];
 
     SDL_Rect src;
     jint texHeight;
     SDL_QueryTexture(eng->textures[sp->d->textureId],
             NULL, NULL, NULL, &texHeight);
-    src.x = sp->d->rect.bl.x;
-    src.y = texHeight - sp->d->rect.tr.y;
-    src.w = sp->d->rect.tr.x - sp->d->rect.bl.x;
-    src.h = sp->d->rect.tr.y - sp->d->rect.bl.y;
+    src.x = sp->d->rect.bl.v[0];
+    src.y = texHeight - sp->d->rect.tr.v[1];
+    src.w = sp->d->rect.tr.v[0] - sp->d->rect.bl.v[0];
+    src.h = sp->d->rect.tr.v[0] - sp->d->rect.bl.v[1];
 
     SDL_RenderCopy (eng->renderer, eng->textures[sp->d->textureId],
             &src, &dest);
